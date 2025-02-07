7bb2cd8d307379adec6a54c4668f55bb39c152a6
jomaway <jomaway@users.noreply.github.com>
2025-02-08 00:54:44 +0100

    refactor: move point input to header bar.

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use ratatui::layout::Flex;
-use ratatui::widgets::Clear;
 use ratatui::widgets::BorderType;
+use ratatui::widgets::Clear;
 
 use std::io;
 
@@ -14,12 +14,11 @@ use ratatui::{
     symbols::border,
     text::Line,
     widgets::{Block, Borders, Paragraph, Widget},
-    DefaultTerminal, Frame,};
-
+    DefaultTerminal, Frame,
+};
 
 use crate::grade::*;
 use crate::table::GradeTable;
-
 
 #[derive(Debug, PartialEq)]
 pub enum AppState {
@@ -32,6 +31,7 @@ pub enum AppState {
 pub struct App {
     state: AppState,
     table: GradeTable,
+    point_edit_field: InputField,
     data: GradeCalculator,
 }
 
@@ -41,16 +41,15 @@ impl App {
         Self {
             state: AppState::Running,
             table: GradeTable::new(data.calc()),
+            point_edit_field: InputField::new(),
             data,
         }
-
     }
 
     pub fn set_points(&mut self, points: u32) {
         self.data.points = points;
         self.table.data(self.data.calc());
     }
-
 
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while self.state != AppState::Exited {
@@ -80,7 +79,8 @@ impl App {
 
         frame.render_widget(block, area);
 
-        let [header_area,_, main_area, _] = Layout::vertical([
+        // main layout.
+        let [header_area, _, main_area, _] = Layout::vertical([
             Constraint::Length(1),
             Constraint::Length(1),
             Constraint::Length(21),
@@ -88,32 +88,56 @@ impl App {
         ])
         .areas(inner);
 
-        let [table_area, _] = Layout::horizontal([
-            Constraint::Max(80),
-            Constraint::Fill(1),
+        let [table_area, _] =
+            Layout::horizontal([Constraint::Max(80), Constraint::Fill(1)]).areas(main_area);
+
+        // self.render_header(header_area, frame.buffer_mut());
+        let text = format!(" {} ", self.data.scale.text());
+
+        let color = scale_color(&self.data.scale);
+
+        let [scale_identifier_area, input_area, version_area] = Layout::horizontal([
+            Constraint::Min(text.len() as u16),
+            Constraint::Percentage(100),
+            Constraint::Length(12),
         ])
-        .areas(main_area);
+        .areas(header_area);
 
-        self.render_header(header_area, frame.buffer_mut());
-        self.table.render(table_area, frame.buffer_mut());
+        let scale_identifier =
+            Paragraph::new(text).style(Style::default().fg(Color::Black).bg(color));
 
-        
+        let bar_style = Style::default().bg(Color::Rgb(60, 56, 54));
+
+        let version = Paragraph::new("graca v0.1")
+            .right_aligned()
+            .style(bar_style);
+
+        frame.render_widget(scale_identifier, scale_identifier_area); 
+        frame.render_widget(version, version_area); 
+
         if self.state == AppState::RunningEditPoints {
-            let point_block = Paragraph::new(format!("{}", self.data.points))
-            .style(Style::default().fg(Color::Yellow))
-            .block(
-                Block::default()
-                    .borders(Borders::ALL)
-                    .border_type(BorderType::Rounded)
-                    .title("Max Points:"),
-            );
+            let input = Paragraph::new(format!(" max:{}",self.point_edit_field.input.as_str()))
+                .style(bar_style.fg(Color::Yellow));
 
-            let area = popup_area(area, 60, 20);
-            frame.render_widget(Clear, area);
-            frame.render_widget(point_block, area);
+            frame.render_widget(input, input_area); 
+
+            // Make the cursor visible and ask ratatui to put it at the specified coordinates after rendering
+            #[allow(clippy::cast_possible_truncation)]
+            frame.set_cursor_position(Position::new(
+                // Draw the cursor at the current position in the input field.
+                // This position is can be controlled via the left and right arrow key
+                // the plus 5 comes from the max: which is printed before.
+                input_area.x + self.point_edit_field.character_index as u16 + 5,
+                input_area.y,
+            ))
+        } else {
+            frame.render_widget(Paragraph::new("").style(bar_style), input_area);
         }
 
+        self.table.render(table_area, frame.buffer_mut());
+
     }
+
 
     fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
@@ -128,69 +152,50 @@ impl App {
     }
 
     fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char(c) if c.is_digit(10) => {
-                if self.state == AppState::RunningEditPoints && self.data.points < 100 {
-                    self.set_points(self.data.points * 10 + c.to_digit(10).unwrap());
+        match self.state {
+            AppState::Running => match key_event.code {
+                KeyCode::Char('j') | KeyCode::Down => self.table.next_row(),
+                KeyCode::Char('k') | KeyCode::Up => self.table.previous_row(),
+                KeyCode::Char('p') => self.state = AppState::RunningEditPoints,
+                KeyCode::Char('I') => self.data.scale = GradeScale::IHK,
+                KeyCode::Char('T') => self.data.scale = GradeScale::TECHNIKER,
+                KeyCode::Char('L') => self.data.scale = GradeScale::LINEAR,
+                KeyCode::Char('C') => {
+                    // open dialog to add a custom scale
+                    todo!()
                 }
-            }
-            KeyCode::Backspace => {
-                if self.state == AppState::RunningEditPoints {
-                    self.set_points(self.data.points / 10 );
-                }
-            }
-            KeyCode::Char('j') | KeyCode::Down => self.table.next_row(),
-            KeyCode::Char('k') | KeyCode::Up => self.table.previous_row(),
-            KeyCode::Char('p') => self.state = AppState::RunningEditPoints,
-            KeyCode::Char('I') => self.data.scale = GradeScale::IHK,
-            KeyCode::Char('T') => self.data.scale = GradeScale::TECHNIKER,
-            KeyCode::Char('L') => self.data.scale = GradeScale::LINEAR,
-            KeyCode::Char('C') => {
-                // open dialog to add a custom scale
-                todo!()
-            },
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Esc | KeyCode::Enter => self.state = AppState::Running,
+                KeyCode::Char('q') => self.exit(),
+                KeyCode::Esc | KeyCode::Enter => self.state = AppState::Running,
 
-            _ => {}
+                _ => {}
+            },
+            AppState::RunningEditPoints => match key_event.code {
+                KeyCode::Char(c) if c.is_digit(10) => self.point_edit_field.enter_char(c),
+                KeyCode::Backspace => self.point_edit_field.delete_char(),
+                KeyCode::Left => self.point_edit_field.move_cursor_left(),
+                KeyCode::Right => self.point_edit_field.move_cursor_right(),
+                KeyCode::Esc => self.state = AppState::Running,
+                KeyCode::Enter => {
+                    let points = self.point_edit_field.get_number();
+                    self.set_points(points);
+                    self.state = AppState::Running;
+                }
+
+                _ => {}
+            },
+            AppState::RunningShowHelp => todo!(),
+            AppState::Exited => todo!(),
         }
     }
 
-    fn render_header(&mut self, area: Rect, buf: &mut Buffer) {
-        let text = format!(" {} ", self.data.scale.text());
-
-        let color = scale_color(&self.data.scale);
-
-        let [scale_identifier_area, bar_area] =
-        Layout::horizontal([Constraint::Min(text.len() as u16), Constraint::Percentage(100)])
-            .areas(area);
-
-        let scale_identifier = Paragraph::new(text)
-            .style(Style::default().fg(Color::Black).bg(color));
-
-        let bar = Paragraph::new("graca v0.1").right_aligned().style(Style::default().bg(Color::Rgb(60, 56, 54)));
-        scale_identifier.render(scale_identifier_area, buf);
-        bar.render(bar_area, buf);
-
-
-    }
 
 
     fn exit(&mut self) {
         self.state = AppState::Exited
     }
-
 }
 
 
-/// helper function to create a centered rect using up certain percentage of the available rect `r`
-fn popup_area(area: Rect, percent_x: u16, percent_y: u16) -> Rect {
-    let vertical = Layout::vertical([Constraint::Percentage(percent_y)]).flex(Flex::Center);
-    let horizontal = Layout::horizontal([Constraint::Percentage(percent_x)]).flex(Flex::Center);
-    let [area] = vertical.areas(area);
-    let [area] = horizontal.areas(area);
-    area
-}
 
 /// helper function to get scale colors
 fn scale_color(scale: &GradeScale) -> Color {
@@ -199,5 +204,89 @@ fn scale_color(scale: &GradeScale) -> Color {
         GradeScale::TECHNIKER => Color::Blue,
         GradeScale::LINEAR => Color::Green,
         GradeScale::CUSTOM(_) => Color::Red,
+    }
+}
+
+pub struct InputField {
+    input: String,
+    character_index: usize,
+}
+
+impl InputField {
+    const fn new() -> Self {
+        Self {
+            input: String::new(),
+            character_index: 0,
+        }
+    }
+
+    fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    fn enter_char(&mut self, new_char: char) {
+        // add guard to max insert 10 digits.
+        if self.input.len() < 10 {
+            let index = self.byte_index();
+            self.input.insert(index, new_char);
+            self.move_cursor_right();
+        }
+    }
+
+    /// Returns the byte index based on the character position.
+    ///
+    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
+    /// the byte index based on the index of the character.
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            // Method "remove" is not used on the saved text for deleting the selected char.
+            // Reason: Using remove on String works on bytes instead of the chars.
+            // Using remove would require special care because of char boundaries.
+
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+
+    fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
+
+    /// return the input as number
+    /// todo: split returning the value and converting to a number into seperate things.
+    fn get_number(&mut self) -> u32 {
+        let mut number: u32 = self.input.parse().expect("Not a valid number");
+        self.input.clear();
+        self.reset_cursor();
+        number
     }
 }
